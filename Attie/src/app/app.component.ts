5f07a594599a79c57d570063d3fa4e529b9ebdc8
patoattie <patoattie@gmail.com>
2019-08-20 19:05:27 -0300

    Clase 1 proyecto Angular

--- Attie/src/app/app.component.ts
+++ Attie/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Usuario } from './clases/usuario';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent
+export class AppComponent implements OnInit
 {
   unUsuario:Usuario;
   title:string;
