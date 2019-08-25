144a39b05bc2437c44c48223d4b023972b70786f
patoattie <patoattie@gmail.com>
2019-08-24 23:21:08 -0300

    Tour of Heroes

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component(
 {
@@ -10,14 +11,15 @@ import { Hero } from '../hero';
 
 export class HeroesComponent implements OnInit
 {
-	hero:Hero = 
-	{
-		id: 1,
-		name: 'Windstorm'
-	};
+	heroes = HEROES;
+	selectedHero:Hero;
 
 	constructor() {	}
 
 	ngOnInit() { }
 
+	onSelect(hero:Hero):void
+	{
+		this.selectedHero = hero;
+	}
 }
