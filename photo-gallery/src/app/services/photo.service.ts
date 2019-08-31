bf3e208a9405d9845fcf6f3916bc6fdf02e5d3d0
patoattie <patoattie@gmail.com>
2019-08-31 17:08:12 -0300

    Probando ionic

---
+++ photo-gallery/src/app/services/photo.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Photo } from '../clases/photo';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService 
+{
+	public photos: Photo[] = [];
+
+  constructor(private camera: Camera) { }
+
+  takePicture()
+  {
+    const options: CameraOptions = 
+    {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+	this.camera.getPicture(options).then((imageData) => {
+	    // Add new photo to gallery
+	    this.photos.unshift({
+	        data: 'data:image/jpeg;base64,' + imageData
+	    }); }, (err) => {
+	    // Handle error
+	    console.log("Camera issue: " + err);
+	});
+  }
+}
