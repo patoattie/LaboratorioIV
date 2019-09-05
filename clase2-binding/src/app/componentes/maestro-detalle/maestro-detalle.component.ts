36d6e36d0a2feb55d47f101c2a22fba714889cd9
patoattie <patoattie@gmail.com>
2019-09-05 18:50:21 -0300

    Corregido por Lu y Nano

--- clase2-binding/src/app/componentes/maestro-detalle/maestro-detalle.component.ts
+++ clase2-binding/src/app/componentes/maestro-detalle/maestro-detalle.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/clases/usuario';
-import { ListadoComponent } from "src/app/componentes/listado/listado.component";
 
 @Component({
   selector: 'app-maestro-detalle',
